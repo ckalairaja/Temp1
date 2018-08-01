55540ed9594d8fd751c510b328ea993f1e69dca8
Kal <kalairaja@yahoo.com>
2018-08-01 00:02:13 -0700

    Adding files to master to compelte first CV program

--- temp1.py
+++ temp1.py
@@ -2,3 +2,17 @@ import cv2 as cv
 
 print ("Imported CV2")
 
+img = cv.imread("notepad_pic.png")
+print "read image file size = {}x{}".format(img.shape[1], img.shape[0])
+cv.imshow("Original....", img)
+
+img_smooth = cv.GaussianBlur(img, (7,7),0)
+cv.imshow("Smoothed....", img_smooth)
+
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("Grayed....", img_gray)
+
+img_edges = cv.Canny(img_gray, 30, 100)
+cv.imshow("Edges....", img_edges)
+cv.waitKey()
+
